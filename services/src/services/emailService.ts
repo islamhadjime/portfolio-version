134c912cd9b62d1387c0c489292f386c25d1bcd7
islamhadjime <islam.hadjime@gmail.com>
2026-05-23 12:04:19 +0300

    fix:email-falile

--- services/src/services/emailService.ts
+++ services/src/services/emailService.ts
@@ -10,7 +10,7 @@ const transporter = nodemailer.createTransport({
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
   },
-  // РЕШЕНИЕ ПРОБЛЕМЫ
+  family: 4,
   tls: {
     rejectUnauthorized: false
   },
