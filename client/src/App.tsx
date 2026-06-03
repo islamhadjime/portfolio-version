fd2490a846ef47e4be40c0a6404241b56db81ede
Amir <57444044+islamhadjime@users.noreply.github.com>
2026-06-03 20:04:18 +0300

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,7 +3,6 @@ import Header from './components/Header';
 import Hero from './components/Hero';
 import Experience from './components/Experience';
 import Skills from './components/Skills';
-import Projects from './components/Projects';
 import Workflow from './components/Workflow';
 import Contact from './components/Contact';
 import Quiz from './components/Quiz';
@@ -20,7 +19,6 @@ function App() {
         <Hero />
         <Experience />
         <Skills />
-        {/* <Projects /> */}
         <Workflow />
         <Quiz />
         <Contact />
